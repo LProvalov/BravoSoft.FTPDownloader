f1fada327ad990644046a25d1f464a72c4a83948
lev.provalov-home <lev.provalov@gmail.com>
2019-06-16 19:05:24 +0300

    UserService was added

---
+++ DBDownloader/Services/UserService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace DBDownloader.Services
+{
+    public class UserService
+    {
+
+        private static readonly Lazy<UserService> _instance =
+            new Lazy<UserService>(() => new UserService());
+
+        public static UserService Instance { get { return _instance.Value; } }
+
+        private string username = string.Empty;
+        private string password = string.Empty;
+
+        private UserService()
+        {
+        }
+
+        public void SetUserPassword(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public ICredentials GetNetworkCredential()
+        {
+            return new NetworkCredential(username, password);            
+        }
+    }
+}
