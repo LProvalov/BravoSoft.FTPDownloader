b79ccb9176b7c6dace47fa639c4873989288fbad
lev.provalov-home <lev.provalov@gmail.com>
2019-06-18 00:07:11 +0300

    HttpClient update

--- DBDownloader/Net/HTTP/HttpClient.cs
+++ DBDownloader/Net/HTTP/HttpClient.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net.Http;
-using System.Net;
-using DBDownloader.ConfigReader;
+﻿using DBDownloader.ConfigReader;
 using DBDownloader.Services;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Text;
 
 namespace DBDownloader.Net.HTTP
 {
@@ -41,89 +39,66 @@ namespace DBDownloader.Net.HTTP
             return request;
         }
 
-            private readonly string ftp_user = "user1";
-            private readonly string ftp_password = "1q2w3e";
+        private readonly string ftp_user = "user1";
+        private readonly string ftp_password = "1q2w3e";
 
-            private readonly string http_user = "kodup";
-            private readonly string http_password = "update";
+        private readonly string http_user = "kodup";
+        private readonly string http_password = "update";
 
-            private readonly bool proxy_use = false;
-            private readonly string proxy_address = "";
+        private readonly bool proxy_use = false;
+        private readonly string proxy_address = "";
 
-            private readonly string kodup_endpoint = "/kodup";
-            private readonly string login_endpoint = "/users/login.asp";
+        private readonly string kodup_endpoint = "/kodup";
+        private readonly string login_endpoint = "/users/login.asp";
 
-            private readonly string server_ip = "82.208.93.53";
+        private readonly string server_ip = "82.208.93.53";
 
-            private Cookie authCookie = null;
-            private List<Cookie> add_cookies = new List<Cookie>();
+        private Cookie authCookie = null;
+        private List<Cookie> add_cookies = new List<Cookie>();
+        private KodupPageParser kodupPageParser = new KodupPageParser();
 
-            private HttpWebRequest CreateHttpRequest(Uri uri, string method)
+        private HttpWebRequest CreateHttpRequest(Uri uri, string method)
+        {
+            HttpWebRequest request = WebRequest.Create(uri) as HttpWebRequest;
+            if (proxy_use)
             {
-                HttpWebRequest request = WebRequest.Create(uri) as HttpWebRequest;
-                if (proxy_use)
-                {
-                    request.Proxy = string.IsNullOrEmpty(proxy_address) ?
-                        new WebProxy() : new WebProxy(proxy_address);
-                }
-                request.Method = method;
-                request.Accept = "text/html,application/xhtml+xml,application/xml";
-                request.UserAgent = "KodUp";
-                request.Headers.Add("Accept-Encoding", "gzip, deflate");
-                request.Headers.Add("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7");
-                request.Host = server_ip;
-                if (authCookie != null)
-                {
-                    request.CookieContainer = new CookieContainer();
-                    request.CookieContainer.Add(authCookie);
-                }
-
-                if (proxy_use)
-                {
-                    request.Proxy = string.IsNullOrEmpty(proxy_address) ?
-                        new WebProxy() : new WebProxy(proxy_address);
-
-                }
-
-                return request;
+                request.Proxy = string.IsNullOrEmpty(proxy_address) ?
+                    new WebProxy() : new WebProxy(proxy_address);
+            }
+            request.Method = method;
+            request.Accept = "text/html,application/xhtml+xml,application/xml";
+            request.UserAgent = "KodUp";
+            request.Headers.Add("Accept-Encoding", "gzip, deflate");
+            request.Headers.Add("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7");
+            request.Host = server_ip;
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+            if (authCookie != null)
+            {
+                request.CookieContainer = new CookieContainer();
+                request.CookieContainer.Add(authCookie);
             }
 
-            //public FileStruct[] ListDirectory(string path)
-            //{
-            //    if (string.IsNullOrEmpty(path))
-            //    {
-            //        path = "/";
-            //    }
-            //    if (path[0] != '/') path = "/" + path;
-            //    Uri uri = new Uri(string.Format("{0}/{1}", server_ip, path));
-
-            //    HttpWebRequest request = CreateHttpRequest(uri, WebRequestMethods.Http.Get);
-            //    request.ContentType = "text/html; charset=utf-8";
-            //    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse) {
-
-            //    }            
-            //}
-
-            public FileStruct[] GetListKodup()
+            if (proxy_use)
             {
-                FileStruct[] fileStructs = null;
+                request.Proxy = string.IsNullOrEmpty(proxy_address) ?
+                    new WebProxy() : new WebProxy(proxy_address);
 
-                Uri uri = new Uri(string.Format("http://{0}{1}", server_ip, kodup_endpoint));
-                var request = CreateHttpRequest(uri, WebRequestMethods.Http.Get);
+            }
 
-                try
+            return request;
+        }
+
+        public FileStruct[] GetListKodup()
+        {
+            FileStruct[] fileStructs = null;
+
+            Uri uri = new Uri(string.Format("http://{0}{1}", server_ip, kodup_endpoint));
+            var request = CreateHttpRequest(uri, WebRequestMethods.Http.Get);
+
+            try
+            {
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
                 {
-                    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-                    {
-                        Console.WriteLine("{0}", response.StatusCode);
-                    }
-                }
-                catch (WebException wEx)
-                {
-                    Console.WriteLine(wEx.Message);
-
-                    HttpWebResponse response = (HttpWebResponse)wEx.Response;
-
                     Stream receiveStream = response.GetResponseStream();
                     StreamReader readStream = null;
 
@@ -135,79 +110,74 @@ namespace DBDownloader.Net.HTTP
                     {
                         readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
                     }
-                    string data = readStream.ReadToEnd();
-
-
-                    response.Close();
+                    kodupPageParser.Load(readStream);
                     readStream.Close();
                 }
-
-                return fileStructs;
-                //using (WebClient client = new WebClient())
-                //{
-                //    //client.Credentials = new NetworkCredential(http_user, http_password);
-                //    try
-                //    {
-                //        string responseString = client.DownloadString(uri);
-                //        Console.Write(responseString);
-                //    }
-                //    catch (WebException wEx)
-                //    {
-                //        Console.WriteLine(wEx.Message);
-                //    }
-                //}
+            }
+            catch (WebException wEx)
+            {
+                Console.WriteLine(wEx.Message);
+                HttpWebResponse response = (HttpWebResponse)wEx.Response;
+                if (response.StatusCode == HttpStatusCode.Forbidden)
+                {
+                    Console.WriteLine("Forbidden! Wrong user/password pair.");
+                }
+                response.Close();
             }
 
-            public void Login()
-            {
-                string postData = string.Format("user={0}&pass={1}&path={2}",
-                    http_user, http_password, kodup_endpoint);
-                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
-                Uri uri = new Uri(string.Format("http://{0}{1}", server_ip, login_endpoint));
-                HttpWebRequest request = CreateHttpRequest(uri, WebRequestMethods.Http.Post);
-                request.Accept = "application/json, text/javascript, */*";
-                request.ContentType = "application/x-www-form-urlencoded";
-                request.ContentLength = byteArray.Length;
+            return fileStructs;
+        }
 
-                Stream dataStream = request.GetRequestStream();
-                dataStream.Write(byteArray, 0, byteArray.Length);
-                dataStream.Close();
-                string auth = string.Empty;
-                try
+        private void GetAuthCookie(string path_endpoint)
+        {
+            string postData = string.Format("user={0}&pass={1}&path={2}",
+                http_user, http_password, path_endpoint);
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            Uri uri = new Uri(string.Format("http://{0}{1}", server_ip, login_endpoint));
+            HttpWebRequest request = CreateHttpRequest(uri, WebRequestMethods.Http.Post);
+            request.Accept = "application/json, text/javascript, */*";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteArray.Length;
+
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            string auth = string.Empty;
+            try
+            {
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
                 {
-                    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                    Console.WriteLine("Status description: {0}", response.StatusDescription);
+                    using (dataStream = response.GetResponseStream())
                     {
-                        Console.WriteLine("Status description: {0}", response.StatusDescription);
-                        using (dataStream = response.GetResponseStream())
+                        StreamReader reader = new StreamReader(dataStream);
+                        auth = reader.ReadToEnd();
+                        Console.WriteLine(auth);
+                        var set_cookie = response.Headers["Set-Cookie"];
+                        if (set_cookie != null)
                         {
-                            StreamReader reader = new StreamReader(dataStream);
-                            auth = reader.ReadToEnd();
-                            Console.WriteLine(auth);
-                            var set_cookie = response.Headers["Set-Cookie"];
-                            if (set_cookie != null)
+                            foreach (var item in set_cookie.Split(';'))
                             {
-                                foreach (var item in set_cookie.Split(';'))
+                                string[] item_values = item.Trim().Split('=');
+                                if (item_values[0].Equals("Auth"))
                                 {
-                                    string[] item_values = item.Trim().Split('=');
-                                    if (item_values[0].Equals("Auth"))
-                                    {
-                                        authCookie = new Cookie("Auth", item_values[1]) { Domain = request.Host };
-                                    }
-                                    else
-                                    {
-                                        add_cookies.Add(new Cookie(item_values[0], item_values[1]) { Domain = request.Host });
-                                    }
+                                    authCookie = new Cookie("Auth", item_values[1]) { Domain = request.Host };
+                                }
+                                else
+                                {
+                                    add_cookies.Add(new Cookie(item_values[0], item_values[1]) { Domain = request.Host });
                                 }
                             }
                         }
                     }
                 }
-                catch (WebException wEx)
-                {
-                    Console.WriteLine(wEx.Message);
-                    authCookie = null;
-                }
             }
-        
+            catch (WebException wEx)
+            {
+                Console.WriteLine(wEx.Message);
+                authCookie = null;
+            }
+        }
+
     }
 }
---
+++ DBDownloader/Net/HTTP/KodupPageParser.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DBDownloader.Net.HTTP
+{
+    public class KodupPageParser
+    {
+        private readonly int SKIP_HTML_LINE = 5;
+        private readonly string hrefPattern = @"<A HREF="".*"">";
+        private readonly string namePattern = @"\b>.*<";
+        private readonly string lastdatePattern = @"\d{2}-\D*-\d{4} \d{2}:\d{2}:\d{2}";
+        private readonly string sizePattern = @"\d{2}-\D*-\d{4} \d{2}:\d{2}:\d{2}.?\d*";
+        private List<KodupPageFileStruct> databaseFilesList = new List<KodupPageFileStruct>();
+
+        public void Load(StreamReader readStream)
+        {
+            int skipCount = SKIP_HTML_LINE;
+            while (readStream.Peek() >= 0)
+            {
+                string line = readStream.ReadLine().Trim();
+                if (skipCount > 0)
+                {
+                    skipCount--;
+                    continue;
+                }
+                try
+                {
+                    string hrefValue = string.Empty;
+                    string nameValue = string.Empty;
+                    DateTime lastModifiedValue = new DateTime(0);
+                    long sizeValue = 0;
+                    Match hrefResult = Regex.Match(line, hrefPattern);
+                    if (hrefResult.Success)
+                    {
+                        var l = hrefResult.Value.Length;
+                        int skipL = 9;
+                        hrefValue = hrefResult.Value.Substring(skipL, l - skipL - 2);
+                    }
+
+                    Match nameResult = Regex.Match(line, namePattern);
+                    if (nameResult.Success)
+                    {
+                        nameValue = nameResult.Value;
+                        nameValue = nameValue.Substring(1, nameValue.Length - 2).Trim();
+                    }
+
+                    Match sizeResult = Regex.Match(line, sizePattern);
+                    if (sizeResult.Success)
+                    {
+                        string couple = sizeResult.Value;
+                        Match lastDateResult = Regex.Match(couple, lastdatePattern);
+                        if (lastDateResult.Success)
+                        {
+                            lastModifiedValue = DateTime.Parse(lastDateResult.Value);
+                            var start = lastDateResult.Value.Length;
+                            var length = couple.Length - lastDateResult.Value.Length;
+                            string sizeStr = couple.Substring(start, length);
+                            sizeValue = long.Parse(sizeStr.Trim());
+                        }
+                    }
+
+                    databaseFilesList.Add(new KodupPageFileStruct()
+                    {
+                        Name = nameValue,
+                        HREF = hrefValue,
+                        Size = sizeValue,
+                        LastModified = lastModifiedValue
+                    });
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+            }
+        }
+
+        public KodupPageFileStruct[] GetDatabaseFileList()
+        {
+            return databaseFilesList.ToArray();
+        }
+    }
+
+    public struct KodupPageFileStruct
+    {
+        public string HREF;
+        public string Name;
+        public DateTime LastModified;
+        public double Size;
+    }
+}
